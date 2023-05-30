1256163b512a64dae2b0534c8dfc504e108c0a72
maghe <lorenzo.magherini.00@gmail.com>
2023-05-30 13:50:36 +0200

    and there we go

--- build_container_image.sh
+++ build_container_image.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
 #stop on first script error
-
 set -e
 
 cd build
+wget https://github.com/m4gh3/qemu-ci-cd/releases/download/release.2023.05.28/qemu-aarch64-static
+chmod +x qemu-aarch64-static
+cp ../res/* .
+
 ../build_cross_sysroot.sh
 mkdir cross_sysroot; cd cross_sysroot
 tar xf ../cross_sysroot.tar
 cd ..
+
 ../build_gcc_target.sh
 cd install
 tar cf ../install.tar .
 cd ..
-cp ../res/* .
+
 podman build --arch aarch64 -t cross_builder_aarch64 .
--- build_cross_sysroot.sh
+++ build_cross_sysroot.sh
@@ -4,6 +4,6 @@
 #stop on first script error
 set -e
 
-CONTAINER_NAME=$(podman run --arch aarch64 -d debian:experimental sh -c "apt update && yes | apt install linux-libc-dev libc-dev symlinks; symlinks -rc /usr")
+CONTAINER_NAME=$(podman run --arch aarch64 -v$(pwd)/qemu-aarch64-static:/usr/bin/qemu-aarch64-static -d debian:experimental /usr/bin/qemu-aarch64-static /bin/sh -c "apt update && yes | apt install linux-libc-dev libc-dev symlinks; symlinks -rc /usr")
 podman logs -f $CONTAINER_NAME
 podman export $CONTAINER_NAME -o cross_sysroot.tar
